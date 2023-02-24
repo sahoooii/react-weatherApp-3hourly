2e775b5d750e2a6b011222dffc79784b8c8a8e61
sahoooii <saho.chu@gmail.com>
2023-02-24 12:06:53 +0900

    changed bg-color system, added on fetchWeather

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ import { useEffect, useState } from 'react';
 import getFormattedThreeHoursWeather from './services/ForecastService';
 
 function App() {
+	const coldBg = 'from-cyan-700 to-blue-700';
+	const hotBg = 'from-yellow-700 to-orange-700';
+
 	const [query, setQuery] = useState({ q: 'tokyo' });
 	const [units, setUnits] = useState('metric'); //°C ?°F
 	const [weather, setWeather] = useState(null);
 	const [threeHourWeather, setThreeHourWeather] = useState(null); //for 3h forecastData[0]=listを入れていく
+	const [bg, setBg] = useState(coldBg); //BgColor change
 
 	useEffect(() => {
 		const fetchWeather = async () => {
@@ -28,29 +32,38 @@ function App() {
 				setWeather(allTheData);
 
 				setThreeHourWeather(forecastData[0]);
+
+				//BgColor change
+				const range = units === 'metric' ? 25 : 77;
+
+				if (currentData.temp <= range) {
+					setBg(coldBg);
+				} else {
+					setBg(hotBg);
+				}
 			});
 		};
 
 		fetchWeather();
 	}, [query, units]);
 
-	const formatBackgroundColor = () => {
-		//default color
-		if (!weather) {
-			return 'from-cyan-700 to-blue-700';
-		}
+	// const formatBackgroundColor = () => {
+	// 	//default color
+	// 	if (!weather) {
+	// 		return 'from-cyan-700 to-blue-700';
+	// 	}
 
-		const range = units === 'metric' ? 25 : 77;
+	// 	const range = units === 'metric' ? 25 : 77;
 
-		if (weather.temp <= range) {
-			return 'from-cyan-700 to-blue-700';
-		}
-		return 'from-yellow-700 to-orange-700';
-	};
+	// 	if (weather.temp <= range) {
+	// 		return 'from-cyan-700 to-blue-700';
+	// 	}
+	// 	return 'from-yellow-700 to-orange-700';
+	// };
 
 	return (
 		<div
-			className={`mx-auto max-w-screen-md w-[90%] mt-10 mb-10 py-5 px-10 md:px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${formatBackgroundColor()}`}
+			className={`mx-auto max-w-screen-md w-[90%] mt-10 mb-10 py-5 px-10 md:px-32 bg-gradient-to-br h-fit shadow-xl shadow-gray-400 ${bg}`}
 		>
 			<TopButton setQuery={setQuery} />
 			<Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
