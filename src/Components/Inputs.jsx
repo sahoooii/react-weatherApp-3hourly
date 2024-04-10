b73eecd610f9ddc4dbd3ad00827e330bc8522d21
sahoooii <saho.chu@gmail.com>
2024-04-10 16:22:04 +0900

    Refactoring to degree section, add home button, and add form to input section, to search press enter

--- src/Components/Inputs.jsx
+++ src/Components/Inputs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons';
+import { UilSearch, UilLocationPoint, UilHome } from '@iconscout/react-unicons';
 import { MdOutlineClear } from 'react-icons/md';
 import { useState } from 'react';
 import useMediaQuery from '../hooks/useMediaQuery';
@@ -8,14 +8,14 @@ const Inputs = ({ setQuery, units, setUnits }) => {
 	const isAboveMediumScreens = useMediaQuery('(min-width: 768px)');
 
 	const [city, setCity] = useState('');
-	const [changedC, setChangedC] = useState(false);
-	// const [changedF, setChangedF] = useState(true);
+	const [isCelsius, setIsCelsius] = useState(true);
 
-	const handleSearchClick = (e) => {
+	const handleSearch = (e) => {
 		e.preventDefault();
 
 		if (city !== '') {
 			setQuery({ q: city });
+			setCity('');
 		}
 	};
 
@@ -30,6 +30,11 @@ const Inputs = ({ setQuery, units, setUnits }) => {
 		}
 	};
 
+	// Handle Degree
+	const selectedDegree = 'text-3xl font-bold text-white';
+	const notSelectedDegree =
+		'text-xl text-white font-light transition ease-out hover:scale-125 duration-500';
+
 	//metric or imperial
 	const handleUnitsChange = (e) => {
 		const selectedUnit = e.currentTarget.name;
@@ -39,13 +44,16 @@ const Inputs = ({ setQuery, units, setUnits }) => {
 	};
 
 	//onClick °C ? °F
-	const changeFontToggle = () => {
-		setChangedC(!changedC);
+	const changeUnits = () => {
+		setIsCelsius(!isCelsius);
 	};
 
 	return (
 		<div className='md:flex flex-row my-6'>
-			<div className='flex flex-row lg:w-3/4 md:w-4/5 items-center justify-center space-x-2 md:space-x-4'>
+			<form
+				onSubmit={handleSearch}
+				className='flex flex-row lg:w-3/4 md:w-4/5 items-center justify-center space-x-2 md:space-x-4'
+			>
 				<div className='relative'>
 					<input
 						value={city}
@@ -56,54 +64,50 @@ const Inputs = ({ setQuery, units, setUnits }) => {
 					/>
 					<button
 						className='absolute items-center top-0 bottom-0 right-3.5 text-gray-500'
-						onClick={() => setCity('')}
+						// onClick={() => setCity('')}
 					>
-						<MdOutlineClear size={18} />
+						<MdOutlineClear size={18} onClick={() => setCity('')} />
 					</button>
 				</div>
 				<UilSearch
 					size={isAboveMediumScreens ? 24 : 20}
 					className='text-white cursor-pointer transition ease-out hover:scale-125'
-					onClick={handleSearchClick}
+					onClick={handleSearch}
 				/>
 				<UilLocationPoint
 					size={isAboveMediumScreens ? 24 : 20}
 					className='text-white cursor-pointer transition ease-out hover:scale-125'
 					onClick={handleCurrentLocationClick}
 				/>
-			</div>
+				<UilHome
+					size={isAboveMediumScreens ? 24 : 20}
+					className='text-white cursor-pointer transition ease-out hover:scale-125'
+					onClick={() => setQuery({ q: 'tokyo' })}
+				/>
+			</form>
 
 			<div className='flex flex-row lg:w-1/4 md:w-1/5 items-center justify-center mx-auto mt-2 md:mt-0'>
-				{changedC ? (
-					<button
-						name='metric'
-						className='text-xl text-white font-light transition ease-out hover:scale-125'
-						onClick={(e) => {
-							handleUnitsChange(e);
-							changeFontToggle();
-						}}
-					>
-						°C
-					</button>
-				) : (
-					<button className='text-3xl font-bold text-white'>°C</button>
-				)}
+				<button
+					name='metric'
+					className={` ${isCelsius ? selectedDegree : notSelectedDegree}`}
+					onClick={(e) => {
+						handleUnitsChange(e);
+						changeUnits();
+					}}
+				>
+					°C
+				</button>
 				<p className='text-xl text-white mx-1'>|</p>
-
-				{changedC ? (
-					<button className='text-3xl font-bold text-white'>°F</button>
-				) : (
-					<button
-						name='imperial'
-						className='text-xl text-white font-light transition ease-out hover:scale-125'
-						onClick={(e) => {
-							handleUnitsChange(e);
-							changeFontToggle();
-						}}
-					>
-						°F
-					</button>
-				)}
+				<button
+					name='imperial'
+					className={`${!isCelsius ? selectedDegree : notSelectedDegree}`}
+					onClick={(e) => {
+						handleUnitsChange(e);
+						changeUnits();
+					}}
+				>
+					°F
+				</button>
 			</div>
 		</div>
 	);
